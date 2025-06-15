8288e4fd4b633d8f69a745b22e8390e823de5f02
Wezirow03 <aganiyazwezirow@gmail.com>
2025-06-15 23:36:08 +0300

    A
    new Comment

---
+++ MyCon_Dic/MyCon_Dic/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyConcurrentDictionary
+{
+   
+    private Dictionary<string, int> data = new Dictionary<string, int>();
+
+   
+    private object myLock = new object();
+
+    
+    public void Add(string name, int grade)
+    {
+        lock (myLock)
+        {
+            if (!data.ContainsKey(name))
+            {
+                data[name] = grade;
+                Console.WriteLine($"{name} added.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} It is have in the Dictionary");
+            }
+        }
+    }
+
+   
+    public int? Get(string name)
+    {
+        lock (myLock)
+        {
+            if (data.ContainsKey(name))
+            {
+                return data[name];
+            }
+            else
+            {
+                Console.WriteLine($"{name} Dont finded.");
+                return null;
+            }
+        }
+    }
+
+   
+    public void Update(string name, int newGrade)
+    {
+        lock (myLock)
+        {
+            if (data.ContainsKey(name))
+            {
+                data[name] = newGrade;
+                Console.WriteLine($"{name} updated.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} dont finded.");
+            }
+        }
+    }
+
+  
+    public void Remove(string name)
+    {
+        lock (myLock)
+        {
+            if (data.ContainsKey(name))
+            {
+                data.Remove(name);
+                Console.WriteLine($"{name} Removed.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} Dont finded.");
+            }
+        }
+    }
+
+   
+    public void PrintAll()
+    {
+        lock (myLock)
+        {
+            Console.WriteLine("All student:");
+            foreach (var pair in data)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MyConcurrentDictionary myDict = new MyConcurrentDictionary();
+
+            myDict.Add("Ali", 90);
+            myDict.Add("Ayşe", 85);
+            myDict.Add("Muhammad", 65);
+            myDict.Add("Madina", 70);
+
+            int? not = myDict.Get("Ali");
+            Console.WriteLine($"Grade of Ali: {not}");
+
+            myDict.Update("Ali", 70);
+            myDict.Remove("Ayşe");
+
+            myDict.PrintAll();
+        }
+    }
+
+}
---
+++ Thread_BST/Thread_BST/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+
+public class TreeNode
+{
+    public int Value;
+    public TreeNode Left;
+    public TreeNode Right;
+
+    public TreeNode(int value)
+    {
+        Value = value;
+    }
+}
+
+public class ThreadSafeBinaryTree
+{
+    private TreeNode root = null;
+    private object treeLock = new object();
+
+    public void Insert(int value)
+    {
+        lock (treeLock)
+        {
+            root = InsertNode(root, value);
+        }
+    }
+
+    private TreeNode InsertNode(TreeNode node, int value)
+    {
+        if (node == null)
+            return new TreeNode(value);
+
+        if (value < node.Value)
+            node.Left = InsertNode(node.Left, value);
+        else if (value > node.Value)
+            node.Right = InsertNode(node.Right, value);
+
+        return node;
+    }
+
+    public bool Contains(int value)
+    {
+        lock (treeLock)
+        {
+            TreeNode current = root;
+            while (current != null)
+            {
+                if (value == current.Value)
+                    return true;
+                else if (value < current.Value)
+                    current = current.Left;
+                else
+                    current = current.Right;
+            }
+            return false;
+        }
+    }
+
+    public void Print()
+    {
+        lock (treeLock)
+        {
+            PrintReq(root);
+        }
+    }
+
+    private void PrintReq(TreeNode node)
+    {
+        if (node == null) return;
+
+        PrintReq(node.Left);
+        Console.WriteLine(node.Value);
+        PrintReq(node.Right);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ThreadSafeBinaryTree tree = new ThreadSafeBinaryTree();
+
+        Thread t1 = new Thread(() =>
+        {
+            for (int i = 1; i <= 9; i += 2)
+                tree.Insert(i);
+        });
+
+
+        Thread t2 = new Thread(() =>
+        {
+            for (int i = 2; i <= 10; i += 2)
+                tree.Insert(i);
+        });
+  
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine("Tree in-order:");
+        tree.Print();
+
+        Console.WriteLine("Is 5 in the tree? " + (tree.Contains(5) ? "Yes" : "No"));
+        Console.WriteLine("Is 11 in the tree? " + (tree.Contains(11) ? "Yes" : "No"));
+    }
+}
